4999af5720089836523561d6d9636fd55aaea8c7
gala377 <r.galczak@gmail.com>
2020-06-25 09:24:58 +0200

    Changed address to a static one

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  authBaseUrl: 'http://localhost/auth',
-  taskBaseUrl: 'http://localhost/task',
-  paymentBaseUrl: 'http://localhost/payment',
+  authBaseUrl: 'http://172.17.0.3/auth',
+  taskBaseUrl: 'http://172.17.0.3/task',
+  paymentBaseUrl: 'http://172.17.0.3/payment',
 };
