ad6bed4a369270670fd629417b629651141573bc
gala377 <r.galczak@gmail.com>
2020-06-25 09:40:51 +0200

    static ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  authBaseUrl: 'http://172.17.0.3/auth',
-  taskBaseUrl: 'http://172.17.0.3/task',
-  paymentBaseUrl: 'http://172.17.0.3/payment',
+  authBaseUrl: 'http://172.17.0.2/auth',
+  taskBaseUrl: 'http://172.17.0.2/task',
+  paymentBaseUrl: 'http://172.17.0.2/payment',
 };
