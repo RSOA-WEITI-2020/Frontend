4068a95d5c944ccb76c982c4eb161d890f2be3b1
Wojciech Gruszka <wojciech@gruszka.eu>
2020-06-25 04:09:40 +0200

    Build fix

--- src/app/modules/main/pages/order-details/order-details.component.ts
+++ src/app/modules/main/pages/order-details/order-details.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/core/model/task.model';
 
 @Component({
   templateUrl: './order-details.component.html',
-  styleUrls: ['./order-details.component.scss']
+  styleUrls: ['./order-details.component.scss'],
 })
 export class OrderDetailsComponent implements OnInit {
+  task: Task;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
